d8485e6f0b3d81bc1cc82580f12d66f0daab3485
fulong123 <2627925386@qq.com>
2018-11-19 21:20:07 +0800

    逆向生成表

--- Pojo/src/main/java/com/dream/shopping/pojo/po/User.java
+++ Pojo/src/main/java/com/dream/shopping/pojo/po/User.java
@@ -20,7 +20,7 @@ public class User implements Serializable {
     private static final long serialVersionUID = 7159250874103984414L;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY) //自动
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //自动生成策略
     private Integer uId;            //用户ID
     @Column
     private String userRealName;    //用户真实姓名
