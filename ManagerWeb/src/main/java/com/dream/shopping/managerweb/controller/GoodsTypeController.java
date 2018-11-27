104fd4e78c5fe1915d948f5af0f7ecd362b61a03
chenqingquan1 <1520377963@qq.com>
2018-11-27 20:05:58 +0800

    商品分类

--- ManagerWeb/src/main/java/com/dream/shopping/managerweb/controller/GoodsTypeController.java
+++ ManagerWeb/src/main/java/com/dream/shopping/managerweb/controller/GoodsTypeController.java
@@ -56,6 +56,6 @@ public class GoodsTypeController {
 
     @RequestMapping("/add")
     public String add(){
-        return "goodsType/addgoodstype";
+        return "goods_type/addgoodstype";
     }
 }
