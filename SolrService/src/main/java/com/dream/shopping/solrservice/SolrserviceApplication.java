035f499ca8d6ccd6546dcef1fec6a564ed1ff549
lh117523 <1175239851@qq.com>
2018-12-10 11:24:40 +0800

    solr

---
+++ SolrService/src/main/java/com/dream/shopping/solrservice/SolrserviceApplication.java
@@ -0,0 +1,12 @@
+package com.dream.shopping.solrservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolrserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SolrserviceApplication.class, args);
+    }
+}
